92904d0fc34f395ed75b555b9eff682ce37e65d0
xfunktastic <ignacio.avendano@alumnos.ucn.cl>
2024-01-27 07:51:41 -0300

    fix: make user migration

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
+            $table->string('rut')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->integer('points');
+            $table->string('role')->default('user');
         });
     }
 
