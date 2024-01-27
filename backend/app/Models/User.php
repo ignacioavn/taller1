1d1e067627f4cdc61071786b26d4478f6f920d7c
xfunktastic <ignacio.avendano@alumnos.ucn.cl>
2024-01-27 08:23:23 -0300

    fix: change user table

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,27 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
+        'rut',
         'email',
-        'password',
+        'points',
+        'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public $timestamps = false;
 }
