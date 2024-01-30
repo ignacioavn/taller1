e4af2608ac35d4cdac5eac6039bcdfbec21a0023
xfunktastic <ignacio.avendano@alumnos.ucn.cl>
2024-01-30 10:40:38 -0300

    fix: change service name

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -5,13 +5,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class AuthService {
 
   private url = "http://127.0.0.1:8000/api";
 
   constructor(private http: HttpClient) {}
 
-  login(loginForm: any){
+  login(loginForm: any): Observable<any> {
     return this.http.post<any>(`${this.url}/login`, loginForm);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
   }
 }
